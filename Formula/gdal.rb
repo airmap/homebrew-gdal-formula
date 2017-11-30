d0ce66dbfcebd0838c63a3396cafa8fd3e19710c
Thomas Voß <thomas@airmap.com>
2017-11-30 17:03:49 +0100

    Rely on source-tarball instead of git tag

--- Formula/gdal.rb
+++ Formula/gdal.rb
@@ -1,7 +1,7 @@
 class Gdal < Formula
   desc "Geospatial Data Abstraction Library"
   homepage "http://www.gdal.org/"
-  head "https://github.com/OSGeo/gdal.git", :branch => "tags/2.2.2"
+  url "http://download.osgeo.org/gdal/2.2.2/gdal-2.2.2.tar.gz"
 
   option "with-complete", "Use additional Homebrew libraries to provide more drivers."
   option "with-opencl", "Build with OpenCL acceleration."
@@ -224,8 +224,6 @@ class Gdal < Formula
   end
 
   def install
-    Dir.chdir("gdal")
-
     if build.with? "libkml"
       resource("libkml").stage do
         # See main `libkml` formula for info on patches
