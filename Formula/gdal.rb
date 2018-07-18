6f5d7ee9ba3fb5684090e50ff14f82f825b78c99
Gilbert Mena <gmena@spark.net>
2018-07-18 13:49:25 -0700

    Update gdal.rb
    test

--- Formula/gdal.rb
+++ Formula/gdal.rb
@@ -63,7 +63,7 @@ class Gdal < Formula
     depends_on "json-c"
   end
 
-  depends_on "java" => ["1.7+", :optional, :build]
+  depends_on :java => ["1.7+", :optional, :build]
 
   if build.with? "swig-java"
     depends_on "ant" => :build
